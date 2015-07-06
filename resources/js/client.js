b8bed12a628914d6cef0b325bf177903f7b789bf
Dimitris Bouzikas <dimimpou@gmail.com>
2015-07-06 12:48:25 +0300

    Changed the way that parameters are passed in order to be parsed as cgi vars

--- resources/js/client.js
+++ resources/js/client.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 		var host = $('#host').val();
 		var sys_type = $('#sys_type').val();
 		var username = $('#username').val();
+		var passwd = $('#passwd').val();
 		var port = $('#port').val();
 		var receiver_port = $('#receiver_port').val();
 						
@@ -13,14 +14,22 @@ $(document).ready(function(){
 				  
 	function connect(smsc_id, host, sys_type, username, passwd, port, receiver_port)
 	{
+		var url = "";
+		
+		// A temporary solution will be removed
+		// after implemented the login process.
+		url += "connect?password=password&";
+		url += "smsc_id="+smsc_id+"&";
+		url += "host="+host+"&";
+		url += "sys_type="+sys_type+"&";
+		url += "username="+username+"&";
+		url += "passwd="+passwd+"&";
+		url += "port="+port+"&";
+		url += "receiver_port="+receiver_port;
+				  
 		$.ajax({
-			url: "connect?password=password",
+			url: url,
 			cache: false,
-			data: ({
-				   smsc_id: smsc_id, host: host, sys_type: sys_type,
-				   username: username, passwd: "password",
-				   port: port, receiver_port: receiver_port
-			}),
 			dataType: "json",
 			type: "POST",
 			beforeSend: function() {
